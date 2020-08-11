be0c7245f37efeabf60badddf8f0c8c5c79516e1
kamal <kamal@Mac-mini-de-kamal.local>
2020-08-11 01:19:21 +0100

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,12 +10,7 @@ Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 // Redux.
-import { createStore, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
-import { HYDRATE, createWrapper } from "next-redux-wrapper";
-import thunkMiddleware from "redux-thunk";
-// Store.
-import combinedReducer from "../redux/reducers/rootReducer";
+import wrapper from "../redux/store";
 
 // Bootstrap.
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -27,43 +22,6 @@ import "../styles.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-// Middleware.
-const bindMiddleware = (middleware: any) => {
-  if (process.env.NODE_ENV !== "production") {
-    const { composeWithDevTools } = require("redux-devtools-extension");
-    // Create loggerMiddleware.
-    const loggerMiddleware = createLogger();
-    return composeWithDevTools(
-      applyMiddleware(
-        ...middleware
-        // loggerMiddleware
-      )
-    );
-  }
-  return applyMiddleware(...middleware);
-};
-
-// Reducer.
-const reducer = (state: any, action: any) => {
-  if (action.type === HYDRATE) {
-    const nextState = {
-      ...state,
-      ...action.payload,
-    };
-    return nextState;
-  } else {
-    return combinedReducer(state, action);
-  }
-};
-
-// Init store.
-const initStore = () => {
-  return createStore(reducer, bindMiddleware([thunkMiddleware]));
-};
-
-// Create a wrapper.
-const wrapper = createWrapper(initStore);
 
 // Create Initial Props.
 MyApp.getInitialProps = async (appContext: AppContext) => {
--- redux/store.ts
+++ redux/store.ts
@@ -1,6 +1,41 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers/rootReducer";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import { HYDRATE, createWrapper } from "next-redux-wrapper";
+import thunkMiddleware from "redux-thunk";
+import combinedReducer from "../redux/reducers/rootReducer";
 
-const store = createStore(rootReducer);
+// Middleware.
+const bindMiddleware = (middleware: any) => {
+  if (process.env.NODE_ENV !== "production") {
+    const { composeWithDevTools } = require("redux-devtools-extension");
+    // Create loggerMiddleware.
+    const loggerMiddleware = createLogger();
+    return composeWithDevTools(
+      applyMiddleware(...middleware, loggerMiddleware)
+    );
+  }
+  return applyMiddleware(...middleware);
+};
 
-export default store;
+// Reducer.
+const reducer = (state: any, action: any) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state,
+      ...action.payload,
+    };
+    return nextState;
+  } else {
+    return combinedReducer(state, action);
+  }
+};
+
+// Init store.
+const initStore = () => {
+  return createStore(reducer, bindMiddleware([thunkMiddleware]));
+};
+
+// Create a wrapper.
+const wrapper = createWrapper(initStore);
+
+export default wrapper;
